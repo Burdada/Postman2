cff9a9614d5f4e758f0f93fb6855365fb929f6f9
Daria Burdakova <d.saharova@inbox.ru>
2022-09-07 20:11:24 +0300

    PostmanEchoTest added

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,33 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+
+
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoData(){
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data") )
+        ;
+    }
+}
